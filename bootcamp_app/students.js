30d7898a5aa5f767c86d507d5c9b2075622896ba
Megan Honey <mjthoney@gmail.com>
2022-10-04 18:05:48 +0000

    Update with parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,16 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-/*
-pool.query is a function that accepts an SQL query as a JavaScript string. Using the ` (backtick), we can write a multi line string like this to make our SQL look nicer. The function then returns a promise that contains our result when the query is successful.
-*/
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || [5]};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+/*
+pool.query is a function that accepts an SQL query as a JavaScript string. Using the ` (backtick), we can write a multi line string like this to make our SQL look nicer. The function then returns a promise that contains our result when the query is successful.
+*/
+pool.query(queryString, values)
 .then(res => {
   // The result (res) is an array of javascript objects
   // console.log(res);
